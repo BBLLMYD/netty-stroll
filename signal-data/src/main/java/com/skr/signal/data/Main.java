92fffaa2e56fe27926f2f71a04197041b0dbd588
BBLLMYD <mmzw0930@gmail.com>
2020-05-29 11:38:35 +0800

    fix

---
+++ signal-data/src/main/java/com/skr/signal/data/Main.java
@@ -0,0 +1,11 @@
+package com.skr.signal.data;
+
+/**
+ * @author mqw
+ * @create 2020-05-28-16:44
+ */
+public class Main {
+
+
+
+}
---
+++ signal-front/src/main/java/com/skr/signal/front/Main.java
@@ -0,0 +1,11 @@
+package com.skr.signal.front;
+
+/**
+ * @author mqw
+ * @create 2020-05-28-16:44
+ */
+public class Main {
+
+
+
+}
