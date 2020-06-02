e3dd98b6833e167319c8ff0b98a088bbb34edaa1
BBLLMYD <mmzw0930@gmail.com>
2020-06-02 19:21:23 +0800

    fix

---
+++ signal-common/src/main/java/com/skr/signal/common/heartbeat/demo/HeartBeatClientHandler.java
@@ -0,0 +1,39 @@
+package com.skr.signal.common.heartbeat.demo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.Date;
+
+/**
+ * @author mqw
+ * @create 2020-06-02-15:58
+ */
+public class HeartBeatClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("激活时间是："+new Date());
+        System.out.println("HeartBeatClientHandler channelActive");
+        ctx.fireChannelActive();
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("停止时间是："+new Date());
+        System.out.println("HeartBeatClientHandler channelInactive");
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String message = (String) msg;
+        System.out.println(message);
+        if (message.equals("Heartbeat")) {
+            ctx.write("has read message from server");
+            ctx.flush();
+        }
+        ReferenceCountUtil.release(msg);
+    }
+
+}
---
+++ signal-common/src/test/java/com/skr/signal/common/test/HeartbeatClient.java
@@ -0,0 +1,9 @@
+package com.skr.signal.common.test;
+
+/**
+ * @author mqw
+ * @create 2020-06-02-17:20
+ */
+public class HeartbeatClient {
+
+}
