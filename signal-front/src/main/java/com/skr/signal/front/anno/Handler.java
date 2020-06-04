067259948b0e0762177718d259ad681f33b7f4e2
mengqingwen <mengqingwen@hetao101.com>
2020-06-04 23:13:04 +0800

    fix

---
+++ signal-front/src/main/java/com/skr/signal/front/anno/Handler.java
@@ -0,0 +1,18 @@
+package com.skr.signal.front.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author mqw
+ * @create 2020-06-04-11:03 下午
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE})
+public @interface Handler {
+
+    String path() default "";
+
+}
---
+++ signal-front/src/main/java/com/skr/signal/front/initialize/HandlerInit.java
@@ -0,0 +1,11 @@
+package com.skr.signal.front.initialize;
+
+/**
+ * @author mqw
+ * @create 2020-06-04-11:12 下午
+ */
+public class HandlerInit {
+
+
+
+}
