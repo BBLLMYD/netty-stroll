0997a93274bed9250e5dd0e6932fa6ef95be188b
BBLLMYD <mmzw0930@gmail.com>
2020-06-14 20:07:58 +0800

    fix

--- signal-core/src/main/java/com/skr/signal/core/initialize/TcpGovernHandler.java
+++ signal-core/src/main/java/com/skr/signal/core/initialize/TcpGovernHandler.java
@@ -4,6 +4,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.nio.charset.Charset;
+
 /**
  * @author mqw
  * @create 2020-06-09-11:51
@@ -12,7 +14,7 @@ public class TcpGovernHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-
+        System.out.println(msg.toString(Charset.forName("UTF-8")));
     }
 
 }
--- signal-route/src/main/java/com/skr/signal/route/handler/RouteHandler.java
+++ signal-route/src/main/java/com/skr/signal/route/handler/RouteHandler.java
@@ -1,8 +1,16 @@
 package com.skr.signal.route.handler;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
 /**
  * @author mqw
  * @create 2020-06-12-13:49
  */
-public class RouteHandler {
+public class RouteHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+
+    }
 }
