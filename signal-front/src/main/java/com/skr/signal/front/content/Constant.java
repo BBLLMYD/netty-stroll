9519e5d09b2e08493d1bad1f0746967f4c9ac77f
mengqingwen <mengqingwen@hetao101.com>
2020-06-14 20:07:58 +0800

    fix

--- signal-front/src/main/java/com/skr/signal/front/content/Constant.java
+++ signal-front/src/main/java/com/skr/signal/front/content/Constant.java
@@ -6,7 +6,7 @@ package com.skr.signal.front.content;
  */
 public class Constant {
 
-    public static String SERVER_ERROR = "服务器异常";
+    public static final String SERVER_ERROR = "服务器异常";
     public static final Integer SUCCESS = 0;
 
 
