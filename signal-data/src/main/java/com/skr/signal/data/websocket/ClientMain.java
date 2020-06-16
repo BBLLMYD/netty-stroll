596032b43a25e13da061bc82108d0a1cc11b78a9
BBLLMYD <mmzw0930@gmail.com>
2020-06-16 22:18:09 +0800

    fix

---
+++ signal-data/src/main/java/com/skr/signal/data/websocket/ClientMain.java
@@ -0,0 +1,8 @@
+package com.skr.signal.data.websocket;
+
+/**
+ * @author mqw
+ * @create 2020-06-16-9:18 下午
+ */
+public class ClientMain {
+}
--- signal-front/README.md
+++ signal-front/README.md
@@ -1,1 +1,5 @@
 # front
+
+accept read handle&write 线程池隔离
+
+
