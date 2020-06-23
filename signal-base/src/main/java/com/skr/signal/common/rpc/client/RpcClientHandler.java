74331863c8e8ae06d17b408e22a977fb92a49d5f
BBLLMYD <mmzw0930@gmail.com>
2020-06-23 13:09:26 +0800

    fix

---
+++ signal-base/src/main/java/com/skr/signal/common/rpc/client/RpcClientHandler.java
@@ -0,0 +1,84 @@
+package com.skr.signal.common.rpc.client;
+
+import com.skr.signal.common.rpc.letter.RpcRequest;
+import com.skr.signal.common.rpc.letter.RpcResponse;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.SocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author mqw
+ * @create 2020-06-22-13:47
+ */
+public class RpcClientHandler extends SimpleChannelInboundHandler<RpcResponse> {
+    private static final Logger logger = LoggerFactory.getLogger(RpcClientHandler.class);
+
+    private ConcurrentHashMap<String, RPCFuture> pendingRPC = new ConcurrentHashMap<>();
+
+    private volatile Channel channel;
+    private SocketAddress remotePeer;
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public SocketAddress getRemotePeer() {
+        return remotePeer;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        this.remotePeer = this.channel.remoteAddress();
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelRegistered(ctx);
+        this.channel = ctx.channel();
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, RpcResponse response) throws Exception {
+        String requestId = response.getTraceId();
+        RPCFuture rpcFuture = pendingRPC.get(requestId);
+        if (rpcFuture != null) {
+            pendingRPC.remove(requestId);
+            rpcFuture.done(response);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error("client caught exception", cause);
+        ctx.close();
+    }
+
+    public void close() {
+        channel.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public RPCFuture sendRequest(RpcRequest request) {
+        final CountDownLatch latch = new CountDownLatch(1);
+        RPCFuture rpcFuture = new RPCFuture(request);
+        pendingRPC.put(request.getTraceId(), rpcFuture);
+        channel.writeAndFlush(request).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                latch.countDown();
+            }
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+
+        return rpcFuture;
+    }
+}
