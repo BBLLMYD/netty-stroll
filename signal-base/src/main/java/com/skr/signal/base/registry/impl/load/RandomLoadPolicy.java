cbf96b3a0fd6d596f77d92b81a9a85bafe600f41
BBLLMYD <mmzw0930@gmail.com>
2020-06-28 18:04:34 +0800

    fix

--- signal-base/src/main/java/com/skr/signal/base/registry/impl/load/RandomLoadPolicy.java
+++ signal-base/src/main/java/com/skr/signal/base/registry/impl/load/RandomLoadPolicy.java
@@ -1,4 +1,4 @@
-package com.skr.signal.base.registry.impl.zookeeper;/**
+package com.skr.signal.base.registry.impl.load;/**
  * @author mqw
  * @create 2020-06-25-15:43
  */
@@ -12,7 +12,7 @@ import java.util.concurrent.ThreadLocalRandom;
  * @auther mqw
  * @date 2020-06-25
  **/
-public class ZKLoadPolicy implements LoadPolicy {
+public class RandomLoadPolicy implements LoadPolicy {
 
     @Override
     public String filterServices(List<String> services) {
--- signal-base/src/test/java/com/skr/signal/common/test/Main.java
+++ signal-base/src/test/java/com/skr/signal/common/test/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        RpcServer instance = RpcServer.getInstance();
+        RpcServer instance = RpcServer.run();
 
     }
 
