dd05183835da3766e6409e7e53150bee47761935
mengqingwen <mengqingwen@hetao101.com>
2020-06-28 16:31:41 +0800

    fix

--- signal-front/src/test/java/com/skr/signal/front/test/Main.java
+++ signal-front/src/test/java/com/skr/signal/front/test/Main.java
@@ -1,10 +1,13 @@
 package com.skr.signal.front.test;
 
+
 /**
  * @author mqw
  * @create 2020-05-30-11:18 上午
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+    }
 
 }
