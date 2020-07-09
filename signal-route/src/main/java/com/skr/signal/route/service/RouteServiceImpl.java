a8e494050ab92485bbc59429802f3601663d7613
mengqingwen <mengqingwen@hetao101.com>
2020-07-09 11:09:48 +0800

    fix

--- signal-route/src/main/java/com/skr/signal/route/service/RouteServiceImpl.java
+++ signal-route/src/main/java/com/skr/signal/route/service/RouteServiceImpl.java
@@ -1,7 +1,9 @@
 package com.skr.signal.route.service;
 
+import com.skr.signal.base.rpc.client.proxy.RpcClient;
 import com.skr.signal.base.rpc.server.anno.RpcServiceTag;
 import com.skr.signal.common.service.ComputeService;
+import com.skr.signal.common.service.DataService;
 import com.skr.signal.common.service.RouteService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,14 +12,14 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2020-07-06
  **/
 @Slf4j
-@RpcServiceTag(targetService = ComputeService.class)
+@RpcServiceTag(targetService = RouteService.class)
 public class RouteServiceImpl implements RouteService {
 
     @Override
     public String route(String args) {
-        String info = "rest from route : " + args + System.currentTimeMillis();
-        log.info(info);
-        return info;
+        String data = RpcClient.getInstance().create(DataService.class).data(args);
+        log.info("RouteService层获取数据获取data层数据："+data);
+        return "[route]" + data + "[route]";
     }
 
 }
